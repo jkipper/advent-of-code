4855d4c50139cd29295f59f90d215fafaf9d3c8d
jkipper <justin.kipper94@gmail.com>
2021-12-06 18:41:02 +0100

    Day 6

---
+++ 6/run.py
@@ -0,0 +1,19 @@
+from collections import deque
+from itertools import chain
+from pathlib import Path
+test_data = Path("test-data.txt").read_text()
+fish = map(int, test_data.split(','))
+
+fish_counter = deque([0]*9)
+
+days = 256 
+for i in fish:
+    fish_counter[i] += 1
+for _  in range(days):
+    fish_counter.rotate(-1)
+    fish_counter[6] +=fish_counter[-1] 
+
+print(sum(fish_counter))
+
+
+
